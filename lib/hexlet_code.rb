78dbe8da6a6b882b8bceebcab77eaf3aaf8bd11d
LichieLich <lichie_lich@mail.ru>
2022-02-28 22:29:58 +0300

    Step 2-3

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 module HexletCode
-  class Error < StandardError; end
-  # Your code goes here...
+  class Tag
+    def self.build(type, **params)
+      preparatory_tag = []
+      if params
+        preparatory_tag << "<#{type}"
+        params.each do |param, value|
+          preparatory_tag << "#{param}=\"#{value}\""
+        end
+      end
+      content = yield if block_given?
+      single_tag?(type) ? "#{preparatory_tag.join(' ')}>" : "#{preparatory_tag.join(' ')}>#{content}</#{type}>"
+    end
+
+    def self.single_tag?(type)
+      %w[area base basefont bgsound br col command embed hr img input
+         isindex keygen link meta param source track wbr].include? type
+    end
+  end
 end
