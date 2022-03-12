a4752482da6f8e2ad22b47046e52892f26b6776a
LichieLich <lichie_lich@mail.ru>
2022-03-13 01:12:15 +0300

    lint  fix again

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,6 +5,22 @@ require_relative 'nested_tag_creator'
 
 module HexletCode
   autoload(:Tag, 'tag')
+  module HexletCode::Tag
+    def self.build(type, **params)
+      preparatory_tag = []
+      if params
+        preparatory_tag << "<#{type}"
+        params.each { |param, value| preparatory_tag << "#{param}=\"#{value}\"" }
+      end
+      content = yield if block_given?
+      single_tag?(type) ? "#{preparatory_tag.join(' ')}>" : "#{preparatory_tag.join(' ')}>#{content}</#{type}>"
+    end
+
+    def self.single_tag?(type)
+      %w[area base basefont bgsound br col command embed hr img input
+         isindex keygen link meta param source track wbr].include? type
+    end
+  end
 
   def self.form_for(user, url: nil, &block)
     nested_tag = block.call(NestedTagCreator.new(user)) if block_given?
