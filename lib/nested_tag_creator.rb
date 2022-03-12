5f76aee3d72e27a1f312f6717bf841f1f4105045
LichieLich <lichie_lich@mail.ru>
2022-03-13 01:01:48 +0300

    Block synth test

--- lib/nested_tag_creator.rb
+++ lib/nested_tag_creator.rb
@@ -12,9 +12,9 @@ class NestedTagCreator
     @all_nested_tags += add_label(attribute)
     @all_nested_tags +=
       if args[:as] == :text
-        "\n  #{HexletCode::Tag.build('textarea', cols: args[:cols] || 20, rows: args[:rows] || 40, name: attribute) do
+        "\n  #{HexletCode::Tag.build('textarea', cols: args[:cols] || 20, rows: args[:rows] || 40, name: attribute) {
                  @user.public_send(attribute)
-               end }"
+               } }"
       else
         args[:name] = attribute
         args[:type] = 'text'
